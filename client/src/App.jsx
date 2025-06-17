e8c2e73a196f4c1c22624c0fbb4008f86f319839
Het Jethva <hetjethva07@gmail.com>
2025-06-17 16:55:59 +0530

    original boilerplate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,7 @@
 function App() {
-
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-
-        team 22 zidio 
-  </h1>
+      <h1 class="text-3xl font-bold underline">hello world</h1>
     </>
   )
 }
