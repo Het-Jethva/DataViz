16d62b8778c39b83289144bc0f889ab62d0fb031
Het Jethva <hetjethva07@gmail.com>
2025-06-20 16:28:56 +0530

    feat: updated App.jsx with authentication routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,42 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+import { Provider } from "react-redux"
+import { store } from "./redux/store"
+import Login from "./components/auth/Login"
+import Signup from "./components/auth/Signup"
+
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">hello world</h1>
-    </>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/signup"
+              element={<Signup />}
+            />
+            <Route
+              path="/"
+              element={
+                <Navigate
+                  to="/login"
+                  replace
+                />
+              }
+            />
+            {/* Add more routes here as you build other features */}
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   )
 }
 
