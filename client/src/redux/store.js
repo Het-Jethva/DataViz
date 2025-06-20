99c675bf922cd496368b72aeb916fc5476bb80f2
Het Jethva <hetjethva07@gmail.com>
2025-06-20 16:27:54 +0530

    feat: update Redux store with auth reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
+import authReducer from "./slices/authSlice.js"
 
 export const store = configureStore({
   reducer: {
-    // add reducers here
+    auth: authReducer,
   },
 })
