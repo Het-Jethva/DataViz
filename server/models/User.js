25ea6a6f582e546a3fee530e30c8adc95df43967
Het Jethva <hetjethva07@gmail.com>
2025-06-23 23:24:06 +0530

    perf(models): add strategic database indexes to User schema
    Add indexes on frequently queried fields (email, role, isActive, createdAt)
    to improve database query performance and response times.

--- server/models/User.js
+++ server/models/User.js
@@ -78,4 +78,9 @@ userSchema.methods.toJSON = function () {
   return userObject
 }
 
+userSchema.index({ email: 1 })
+userSchema.index({ role: 1 })
+userSchema.index({ isActive: 1 })
+userSchema.index({ createdAt: -1 })
+
 export default mongoose.model("User", userSchema)
